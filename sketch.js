6ee854c37c44dc204ee43c096f877c90f9242879
switchcrit <62515128+switchcrit@users.noreply.github.com>
2022-12-12 21:14:22 +0530

    Update sketch.js
    made it local

--- sketch.js
+++ sketch.js
@@ -11,13 +11,13 @@ let happyu = 255;
 function setup() {
   // Load the images from files
   img1 = loadImage(
-    "https://freight.cargo.site/t/original/i/46ed58415afe4ac8f63284da3b73ed4aba65cf827609ff57dea9676dfc79e5c6/BG-full.png"
+    "BG%20full.png"
   );
   img2 = loadImage(
-    "https://freight.cargo.site/t/original/i/b2d5e3c154745a6db5db78bbdfb6c972a9f959e753ccc837f80382452b1acb04/img_1.png"
+    "img_1.png"
   );
   img3 = loadImage(
-    "https://freight.cargo.site/t/original/i/fedb11efde65f66b7d459b04c8305289653323bff48106314b21948a688d01e9/img_2.png"
+    "img_2.png"
   );
 
   // Set the starting x position of image 1
